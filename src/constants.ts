9762065cd2e31126d2de5bd60e4fadbcdc2d8efc
Daniel Whiffing <danielwhiffing@gmail.com>
2024-06-19 15:47:14 -0400

    Lower default player count

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export let CONFIG = {
   gravity: 800,
   jumpHeight: -500,
   pipeSpeed: 200,
-  playerCount: 250,
+  playerCount: 100,
   pipeDelay: 3000,
   pipeDistance: 150,
   mutationRate: 0.1,
